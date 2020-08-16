150621df8eeae3332d6f4c26afeeb0785d4663c6
siwangishah <siwangishah@gmail.com>
2020-08-16 18:07:29 +0530

    backup

---
+++ Shuffle_name.py
@@ -0,0 +1,32 @@
+# def Shuffle(firstname, lastname):
+#     firstname, lastname = lastname, firstname
+#     print(firstname, lastname)
+#
+#
+# Shuffle("Nikhil", "Saha")
+
+
+
+
+UserInput = str(input("Enter the name: "))
+Length = len(UserInput)
+print(Length)
+for element in range(len(UserInput)):
+    if UserInput[element] == " ":
+        element
+    else:
+        continue
+if word == 1:
+    UserInput[len(element)-1] = UserInput[0]
+    UserInput[0] = UserInput[len(element)-1]
+print(UserInput[0] + UserInput[len(UserInput)-1])
+
+
+print("All")
+
+print("al")
+
+
+
+
+
